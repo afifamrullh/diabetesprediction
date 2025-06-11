02ad5f2f648e7e207a06ba8711da7776c7e62594
afifamrullh <77236396+afifamrullh@users.noreply.github.com>
2025-06-11 14:37:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ blood_pressure, skin_thickness, insulin, bmi, dpf, age]])
 # Prediksi
   prediction = model.predict(features)[0]
 # Tampilkan hasil
-  if prediction == 1:
-    st.error("Hasil: Positif Diabetes")
-  else:
-    st.success("Hasil: Tidak Diabetes")
+ if prediction == 1:
+   st.error("Hasil: Positif Diabetes")
+ else:
+   st.success("Hasil: Tidak Diabetes")
